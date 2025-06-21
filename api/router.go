876bbd64f3872a60fa074f6bc6ccb713a4322487
Rojhat Sinan Balka <rojbalka@gmail.com>
2025-06-21 19:15:40 +0300

    Add config management

--- api/router.go
+++ api/router.go
@@ -1,14 +1,23 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
-func NewRouter() *gin.Engine {
+func NewRouter() (*gin.Engine, error) {
+	// TODO: set mode to final value before submission
+	//gin.SetMode(gin.ReleaseMode)
+
 	r := gin.Default()
+	if err := r.SetTrustedProxies(nil); err != nil {
+		return nil, fmt.Errorf("failed to set trusted proxies: %v", err)
+	}
 
 	// TODO: add other middlewares if necessary
 
 	r.GET("/ping", Ping)
 	r.POST("/messages", SendMessage)
 
-	return r
+	return r, nil
 }
